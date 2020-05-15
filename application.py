82795b5bbd0da9f332c430be6885a9933389aa19
eduardogcupertino <65354060+eduardogcupertino@users.noreply.github.com>
2020-05-14 21:17:43 -0300

    Update application.py

--- application.py
+++ application.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
-
-@app.route( '/predict', methods=['GET'])
+@app.route( '/predict', methods=['GET', 'POST'])
 def predict():
     return {"Chave": 1}
